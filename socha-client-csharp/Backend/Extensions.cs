8021208de792b4530007e0141459d3ce0a72855e
niklasCarstensen <niklas.carstensen@gmx.de>
2020-10-07 17:52:18 +0200

    Add ToColor()

--- socha-client-csharp/Backend/Extensions.cs
+++ socha-client-csharp/Backend/Extensions.cs
@@ -12,5 +12,20 @@ namespace socha_client_csharp
         public static PlayerTeam OtherTeam(this PlayerTeam t) => t == PlayerTeam.ONE ? PlayerTeam.TWO : PlayerTeam.ONE;
 
         public static PieceColor Next(this PieceColor c, List<PieceColor> OrderedColors) => OrderedColors[(OrderedColors.IndexOf(c) + 1) % OrderedColors.Count];
+
+        public static Color ToColor(this PieceColor? c) 
+        {
+            if (c.HasValue)
+                if (c.Value == PieceColor.BLUE)
+                    return Color.Blue;
+                else if (c.Value == PieceColor.GREEN)
+                    return Color.Green;
+                else if (c.Value == PieceColor.RED)
+                    return Color.Red;
+                else
+                    return Color.Yellow;
+            else
+                return Color.Black;
+        }
     }
 }
