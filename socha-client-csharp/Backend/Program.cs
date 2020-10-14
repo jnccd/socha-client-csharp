dee48812038ec2605b31add7943e40eefa095884
niklasCarstensen <niklas.carstensen@gmx.de>
2020-10-14 22:03:35 +0200

    Enforce naming conventions in Program.cs

--- socha-client-csharp/Backend/Program.cs
+++ socha-client-csharp/Backend/Program.cs
@@ -13,9 +13,9 @@ namespace SochaClient
     public class Program
     {
         // Start Arguments
-        static string Host = "127.0.0.1";
-        static int Port = 13050;
-        static string Reservation = "";
+        static string host = "127.0.0.1";
+        static int port = 13050;
+        static string reservation = "";
         public static string Strategy { get; private set; } = "";
 
         public static bool LogNetwork = true;
@@ -26,15 +26,15 @@ namespace SochaClient
         static StreamWriter logWriter;
 
         public static string RoomID { get; private set; } = "";
-        static Logic PlayerLogic;
-        static State GameState;
+        static Logic playerLogic;
+        static State gameState;
         
         static void Main(string[] args)
         {
             if (!GotProperStartArguments(args)) return;
 
-            GameState = new State();
-            PlayerLogic = new Logic { GameState = GameState };
+            gameState = new State();
+            playerLogic = new Logic { GameState = gameState };
             instanceIdentifier = DateTime.Now.ToBinary().ToString();
             if (logToFile)
                 logWriter = File.CreateText(LogFileName);
@@ -64,10 +64,9 @@ namespace SochaClient
                         case "--help":
                             ConsoleWriteLine($@"
         Usage: start.sh [options]
-          -h, --host:
-              The IP address of the host to connect to (default: {Host}).
+              The IP address of the host to connect to (default: {host}).
           -p, --port:
-              The port used for the connection (default: {Port}).
+              The port used for the connection (default: {port}).
           -r, --reservation:
               The reservation code to join a prepared game.
           -s, --strategy:
@@ -79,19 +78,19 @@ namespace SochaClient
                         case "-h":
                         case "--host":
                             if (args.Length > i + 1)
-                                Host = args[i + 1];
+                                host = args[i + 1];
                             break;
 
                         case "-p":
                         case "--port":
                             if (args.Length > i + 1)
-                                Port = Convert.ToInt32(args[i + 1]);
+                                port = Convert.ToInt32(args[i + 1]);
                             break;
 
                         case "-r":
                         case "--reservation":
                             if (args.Length > i + 1)
-                                Reservation = args[i + 1];
+                                reservation = args[i + 1];
                             break;
 
                         case "-s":
@@ -115,7 +114,7 @@ namespace SochaClient
         {
             try
             {
-                return new TcpClient(Host, Port);
+                return new TcpClient(host, port);
             }
             catch (Exception e)
             {
@@ -129,10 +128,10 @@ namespace SochaClient
         {
             Move LastMove = null;
 
-            if (string.IsNullOrWhiteSpace(Reservation))
+            if (string.IsNullOrWhiteSpace(reservation))
                 Send(stream, $"<protocol><join gameType=\"swc_2021_blokus\" />");
             else
-                Send(stream, $"<protocol><joinPrepared reservationCode=\"{Reservation}\" />");
+                Send(stream, $"<protocol><joinPrepared reservationCode=\"{reservation}\" />");
             
             while (true)
             {
@@ -156,36 +155,36 @@ namespace SochaClient
                     foreach (var r in recievedObjs.Room)
                         if (r.Data != null && r.RoomId == RoomID)
                             if (r.Data.Class == "sc.framework.plugins.protocol.MoveRequest")
-                                Send(stream, (LastMove = PlayerLogic.GetMove()).ToXML());
+                                Send(stream, (LastMove = playerLogic.GetMove()).ToXML());
                             else if (r.Data.Class == "memento")
                             {
                                 var inState = r.Data.State;
 
                                 // Attr
-                                GameState.Turn = inState.Turn;
-                                GameState.Round = inState.Round;
-                                GameState.StartPiece = inState.StartPiece;
-                                GameState.StartTeam = inState.StartTeam.Text;
-                                GameState.OrderedColors = inState.OrderedColors.Color;
-                                GameState.CurrentColorIndex = inState.CurrentColorIndex;
-                                GameState.CurrentColor = (PieceColor)((GameState.CurrentColorIndex % Enum.GetValues(typeof(PieceColor)).Length) + 1);
+                                gameState.Turn = inState.Turn;
+                                gameState.Round = inState.Round;
+                                gameState.StartPiece = inState.StartPiece;
+                                gameState.StartTeam = inState.StartTeam.Text;
+                                gameState.OrderedColors = inState.OrderedColors.Color;
+                                gameState.CurrentColorIndex = inState.CurrentColorIndex;
+                                gameState.CurrentColor = (PieceColor)((gameState.CurrentColorIndex % Enum.GetValues(typeof(PieceColor)).Length) + 1);
 
                                 // Board
-                                GameState.CurrentBoard = new Board();
+                                gameState.CurrentBoard = new Board();
                                 foreach (var f in inState.Board.Field)
-                                    GameState.CurrentBoard.GetField(f.X, f.Y).color = f.Content;
+                                    gameState.CurrentBoard.GetField(f.X, f.Y).color = f.Content;
 
                                 // Piece Inventories
-                                GameState.BlueShapes = inState.BlueShapes.Shape;
-                                GameState.YellowShapes = inState.YellowShapes.Shape;
-                                GameState.RedShapes = inState.RedShapes.Shape;
-                                GameState.GreenShapes = inState.GreenShapes.Shape;
+                                gameState.BlueShapes = inState.BlueShapes.Shape;
+                                gameState.YellowShapes = inState.YellowShapes.Shape;
+                                gameState.RedShapes = inState.RedShapes.Shape;
+                                gameState.GreenShapes = inState.GreenShapes.Shape;
 
                                 // Last moves
                                 if (inState.LastMove?.Class == "sc.plugin2021.SkipMove")
-                                    GameState.LastMove = new SkipMove();
+                                    gameState.LastMove = new SkipMove();
                                 else if (inState.LastMove?.Class == "sc.plugin2021.SetMove")
-                                    GameState.LastMove = new SetMove(
+                                    gameState.LastMove = new SetMove(
                                         inState.LastMove.Piece.Color,
                                         inState.LastMove.Piece.Kind,
                                         inState.LastMove.Piece.Rotation,
@@ -194,15 +193,15 @@ namespace SochaClient
                                         inState.LastMove.Piece.Position.Y);
 
                                 // Names
-                                GameState.FirstPlayerName = inState.First.DisplayName;
-                                GameState.SecondPlayerName = inState.Second.DisplayName;
+                                gameState.FirstPlayerName = inState.First.DisplayName;
+                                gameState.SecondPlayerName = inState.Second.DisplayName;
                                 UpdateConsoleTitle();
 
                                 if (DrawBoard)
                                     DrawBoardPng();
                             }
                             else if (r.Data.Class == "welcomeMessage")
-                                PlayerLogic.MyTeam = r.Data.Color;
+                                playerLogic.MyTeam = r.Data.Color;
 
                 if (LastMove != null && LastMove is SetMove && (LastMove as SetMove).DebugHints.Count > 0)
                     ConsoleWriteLine("Debug Hints from the Last Move:\n" +
@@ -211,7 +210,7 @@ namespace SochaClient
         }
         static void UpdateConsoleTitle()
         {
-            try { Console.Title = PlayerLogic.MyTeam.ToString() + " in " + GameState.FirstPlayerName + " vs " + GameState.SecondPlayerName; } 
+            try { Console.Title = playerLogic.MyTeam.ToString() + " in " + gameState.FirstPlayerName + " vs " + gameState.SecondPlayerName; } 
             catch { }
         }
         static void DrawBoardPng()
@@ -220,7 +219,7 @@ namespace SochaClient
 
             for (int x = 0; x < Board.Width; x++)
                 for (int y = 0; y < Board.Height; y++)
-                    b.SetPixel(x, y, GameState.CurrentBoard.GetField(x, y).color.ToColor());
+                    b.SetPixel(x, y, gameState.CurrentBoard.GetField(x, y).color.ToColor());
 
             try { b.Save("board.png"); } catch {}
         }
