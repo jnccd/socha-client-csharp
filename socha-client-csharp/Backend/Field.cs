17a23807444f22b4be294f169c662818748dca2d
niklasCarstensen <niklas.carstensen@gmx.de>
2021-07-13 17:22:40 +0200

    Update Field

--- socha-client-csharp/Backend/Field.cs
+++ socha-client-csharp/Backend/Field.cs
@@ -6,30 +6,19 @@ namespace SochaClient
 {
     public class Field : ICloneable
     {
-        public PieceColor? color;
-        Board parent;
-        readonly int x, y;
+        public PieceColor? color { get; private set; }
+        public PieceKind? kind { get; private set; }
+        public Board parent { get; private set; }
+        public readonly int x, y;
 
-        public Field(PieceColor? color, Board parent, int x, int y)
+        public Field(PieceColor? color, PieceKind? kind, Board parent, int x, int y)
         {
             this.color = color;
+            this.kind = kind;
             this.parent = parent;
             this.x = x;
             this.y = y;
         }
-
-        public Field[] Get4Neighborhood() => (
-                from p in new Point[] { new Point(x - 1, y), new Point(x + 1, y), new Point(x, y - 1), new Point(x, y + 1) }
-                where parent.IsInBounds(p)
-                select parent.GetField(p)
-            ).ToArray();
-
-        public Field[] Get8Neighborhood() => (
-                from p in new Point[] { new Point(x - 1, y), new Point(x + 1, y), new Point(x, y - 1), new Point(x, y + 1),
-                                        new Point(x - 1, y + 1), new Point(x - 1, y - 1), new Point(x + 1, y + 1), new Point(x + 1, y - 1) }
-                where parent.IsInBounds(p)
-                select parent.GetField(p)
-            ).ToArray();
 
         public object Clone() => (Field)MemberwiseClone();
         public Field CloneWParent(Board cloneParent)
