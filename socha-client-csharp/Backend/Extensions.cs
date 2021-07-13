cc1a024ead1c1a25781fb1b21767af40d88248c4
niklasCarstensen <niklas.carstensen@gmx.de>
2021-07-13 17:18:15 +0200

    Update Extensions

--- socha-client-csharp/Backend/Extensions.cs
+++ socha-client-csharp/Backend/Extensions.cs
@@ -7,21 +7,15 @@ namespace SochaClient
     {
         public static PlayerTeam OtherTeam(this PlayerTeam t) => t == PlayerTeam.ONE ? PlayerTeam.TWO : PlayerTeam.ONE;
 
-        public static PlayerTeam Team(this PieceColor c) => c == PieceColor.BLUE || c == PieceColor.RED ? PlayerTeam.ONE : PlayerTeam.TWO;
-
-        public static PieceColor Next(this PieceColor c, List<PieceColor> OrderedColors) => OrderedColors[(OrderedColors.IndexOf(c) + 1) % OrderedColors.Count];
+        public static PlayerTeam Team(this PieceColor c) => c == PieceColor.RED ? PlayerTeam.ONE : PlayerTeam.TWO;
 
         public static Color ToColor(this PieceColor? c) 
         {
             if (c.HasValue)
                 if (c.Value == PieceColor.BLUE)
                     return Color.Blue;
-                else if (c.Value == PieceColor.GREEN)
-                    return Color.Green;
-                else if (c.Value == PieceColor.RED)
-                    return Color.Red;
                 else
-                    return Color.Yellow;
+                    return Color.Red;
             else
                 return Color.Black;
         }
