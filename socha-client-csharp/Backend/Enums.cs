3d52c04257fec240efd2b712a55532b2f94dbedd
niklasCarstensen <niklas.carstensen@gmx.de>
2021-07-13 17:16:49 +0200

    Update Enums

--- socha-client-csharp/Backend/Enums.cs
+++ socha-client-csharp/Backend/Enums.cs
@@ -8,22 +8,13 @@
     public enum PieceColor 
     { 
         BLUE=1, 
-        YELLOW=2, 
-        RED=3, 
-        GREEN=4 
-    }
-    public enum Rotation 
-    { 
-        NONE, 
-        RIGHT, 
-        MIRROR, 
-        LEFT
+        RED=2, 
     }
     public enum PieceKind
     {
-        MONO, DOMINO,
-        TRIO_L, TRIO_I,
-        TETRO_O, TETRO_T, TETRO_I, TETRO_L, TETRO_Z,
-        PENTO_L, PENTO_T, PENTO_V, PENTO_S, PENTO_Z, PENTO_I, PENTO_P, PENTO_W, PENTO_U, PENTO_R, PENTO_X, PENTO_Y
+        Herzmuschel,
+        Möwe,
+        Seestern,
+        Robbe
     }
 }
