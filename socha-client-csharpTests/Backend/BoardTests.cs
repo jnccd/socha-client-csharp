db6f512cbc70f2405d4ac5f7c9590e7b604e90c5
Niklas Carstensen <niklas.carstensen@outlook.de>
2022-09-26 21:13:24 +0200

    Add board clone unittest

---
+++ socha-client-csharpTests/Backend/BoardTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SochaClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SochaClient.Tests
+{
+    [TestClass()]
+    public class BoardTests
+    {
+        [TestMethod()]
+        public void BoardIsCorrectlyCloned()
+        {
+            Board board = new();
+            board.GetField(1, 1).fishes = 2;
+
+            var clone = (Board)board.Clone();
+            clone.GetField(2, 2).fishes = 3;
+
+            Assert.AreEqual(board.GetField(1, 1).fishes, 2);
+            Assert.AreEqual(board.GetField(2, 2).fishes, 0);
+
+            Assert.AreEqual(clone.GetField(1, 1).fishes, 2);
+            Assert.AreEqual(clone.GetField(2, 2).fishes, 3);
+        }
+    }
+}
