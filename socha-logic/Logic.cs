d64464864b897c5f11174d995cca5ca052b79d44
Niklas Carstensen <kobidogao@yahoo.com>
2024-02-26 15:22:02 +0100

    Revert logic

--- socha-logic/Logic.cs
+++ socha-logic/Logic.cs
@@ -18,34 +18,7 @@ namespace SochaClient
         {
             // TODO: Add your game logic
 
-            var possibleMoves = GameState.GetPossibleMoves();
-
-            var bestScore = -9999; Move bestMove = null;
-            foreach (var possibleMove in possibleMoves.Take(200))
-            {
-                Console.WriteLine($"Handling move {possibleMove}");
-                State futureState = null;
-                try
-                {
-                    futureState = possibleMove.PerformOn(GameState);
-                }
-                catch
-                {
-                    continue;
-                }
-
-                var curScore = futureState.MyselfPlayer.Ship.Pos.q;
-                if (curScore > bestScore)
-                {
-                    bestScore = curScore;
-                    bestMove = possibleMove;
-                }
-            }
-
-            if (bestMove == null)
-                GetHashCode();
-
-            return bestMove ?? possibleMoves.First();
+            return GameState.GetPossibleMoves().First();
         }
     }
 }
